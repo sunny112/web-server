507ce7c00cb0286248c870884f67e18dfb9c67d4
Sunny <ciercier112@gmail.com>
2016-12-19 14:36:04 -0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+
+	logger: function(req, res, next){
+		console.log(req.method + ' ' + req.originalUrl);
+		console.log(new Date().toString());
+		next();
+	}
+};
+
+
+// app.get('/', function(req, res){
+// 	res.send('Hello Express!');
+// });
+app.use(middleware.logger);
+app.get('/about', function(req, res){
+	res.send('About us!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+	console.log(port + 'server startes');
+});
